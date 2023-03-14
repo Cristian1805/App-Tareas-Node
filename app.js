ec8a6a9281b5eae69e38df5427d81e193f01e87b
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-03-14 13:44:18 -0500

    Se agrega el paquete inquirer, donde el usuario podra desplazarse por el menu con las teclas de direccion

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+import colors from 'colors';
+import { inquirerMenu } from './helpers/inquirer.js';
+ 
 console.clear();
-
-
-
-const main = async() => {
-    console.log('Hola mundo')
-
-    let opt = '';
-
-    do {
-        opt = await mostrarMenu();
-        console.log({ opt });
-
-        if(opt !== '0' ) await pausa();
-         
-    } while (opt !=='0');
-
-   
-    // pausa();
-
-}
-
+ 
+const main = async () => {
+  let opt = '';
+  do {
+    opt = await inquirerMenu();
+    console.log({ opt });
+  } while (opt !== '');
+};
+ 
 main();
